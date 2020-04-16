2c5bbce13ec05cca8fa59c910ebdd0ad709a4673
danil0110 <danilmail0110@gmail.com>
2020-04-16 13:05:48 +0300

    Initial commit

---
+++ Laba4_Compress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laba4_Compress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
